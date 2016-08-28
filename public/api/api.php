2ca2c0922c8ab4410f7c4560a29ab49430cebf80
xiang-baidu <shawn@evenhidata.com>
2016-08-28 19:20:14 +0800

    create project

---
+++ public/api/api.php
@@ -0,0 +1,13 @@
+<?php
+    header("content-type:text/html;charset=utf-8");  
+    //$input = $_GET["input"];
+    $input = file_get_contents("php://input");;
+    $m = $_GET["m"];
+    $filename="db.txt";
+    if($m == "get"){
+        echo file_get_contents($filename);
+    }else if($m == "post"){
+        file_put_contents($filename,$input);
+        echo "{\"res\":0}";
+
+    }
